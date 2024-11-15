f57aa01f06caf6ab31bcc12824466f68b7fd58ea
crz29 <crz29@hotmail.com>
2024-11-15 18:16:12 -0300

    cambios en productos.html

--- bucles.js
+++ bucles.js
@@ -3,3 +3,31 @@ console.log(1);
 for (let num = 1; num <= 10; num++){
     console.log(num);
 }
+
+//Constantes: una vez que doy el valor, no puedo modificarlo
+const edad = prompt("Ingrese su edad: ");
+
+if  (edad => 13){
+    console.log("Puede registrarse");
+} else {
+    console.log("No puede registarse");
+}
+console.log(typeof edad)
+
+//Funciones
+//Función sin parámetros que NO devuelve un valor
+function puede(){
+    const edad = parseInt( prompt("Ingrese su edad: "));
+
+if  (edad => 13){
+    console.log("Puede registrarse");
+} else {
+    console.log("No puede registarse");
+}
+console.log(typeof edad)
+}
+puede();
+
+function suma(){
+    
+}
