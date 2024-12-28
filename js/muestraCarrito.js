8738236c1ba4ee41bfd8a68f5d13190da2d4408e
crz29 <crz29@hotmail.com>
2024-12-28 01:15:04 -0300

    script muestraCarrito abajo|modif stylesCarrito.css

--- js/muestraCarrito.js
+++ js/muestraCarrito.js
@@ -6,18 +6,23 @@ const items = document.querySelector(".items");
 items.innerHTML = "";
 
 carrito.forEach((item)  => {
-    const html = `
-       
+    const html = `       
         <tr data-id="${item.id}" class="linea_prod">
-            <td>${item.nombre}</h3>
-            <td>${item.cantidad}</p>
-            <td>$${item.precio}</p>
-            <td>$${item.precio * item.cantidad}</p>
+            <td>${item.nombre}</td>
+            <td>${item.cantidad}</td>
+            <td>$${item.precio}</td>
+            <td>$${item.precio * item.cantidad}</td>
         </tr>
 
     `;
-    items.html += html;
+    items.innerHTML += html;
 
 });
-    
+  //Calcula la suma total de los precios
+  const sumaTotal = carrito.reduce((sum,item) => sum + (item.precio * item.cantidad), 0);
+  const suma = document.getElementById("suma");
+  const htmlSuma = `
+     <p class="sumaTotal">Total: \n \n $${sumaTotal}</p>
+    `;
+    suma.innerHTML = htmlSuma;
 
